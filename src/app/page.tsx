548d1e74ae31d87eacc84355774a5841f3a17174
HardyNuklear <hardy.herinavalona@nuklear.io>
2025-03-11 06:27:02 +0300

    add link to videos and upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import VideoUpload from "@/components/VideoUpload";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <VideoUpload />
+    <div className="flex flex-col gap-5 items-center justify-center min-h-screen p-4">
+      <Link href="/videos">
+        <button className="border border-white rounded-sm px-5 py-2 cursor-pointer">
+          Go to Videos
+        </button>
+      </Link>
+      <Link href="/upload">
+        <button className="border border-white rounded-sm px-5 py-2 cursor-pointer">
+          Go to Upload
+        </button>
+      </Link>
     </div>
   );
 }
